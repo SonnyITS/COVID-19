ea423175c1e133ff316945344c3a0078b61d5789
Yuansun Jiang <yuansun.jiang@gmail.com>
2020-04-06 22:38:21 -0400

    Added a combined CT county data

--- master_us_data_cleaner.py
+++ master_us_data_cleaner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[361]:
+# In[391]:
 
 
 import numpy as np
@@ -12,7 +12,7 @@ import re
 import datetime
 
 
-# In[362]:
+# In[392]:
 
 
 ### Start of importing raw data from github
@@ -31,7 +31,7 @@ dfC
 ### End of importing raw data from github
 
 
-# In[363]:
+# In[393]:
 
 
 ### Start of US population dataframe creation
@@ -63,7 +63,7 @@ df_USPop_State
 ### End of US population dataframe creation
 
 
-# In[364]:
+# In[394]:
 
 
 ### Start of US states confirmed time series
@@ -87,7 +87,7 @@ dfC_US
 ### End of US states confirmed time series
 
 
-# In[365]:
+# In[395]:
 
 
 ### Start of US states confirmed per 100k residents time series
@@ -107,7 +107,7 @@ dfC_perCap
 ### End of US states confirmed per 100k residents time series
 
 
-# In[366]:
+# In[396]:
 
 
 ### Start of US states deaths time series
@@ -131,7 +131,7 @@ dfD_US
 ### End of US states deaths time series
 
 
-# In[367]:
+# In[397]:
 
 
 ### Start of US states deaths per 100k residents time series
@@ -151,7 +151,7 @@ dfD_perCap
 ### End of US states deaths per 100k residents time series
 
 
-# In[368]:
+# In[398]:
 
 
 ### Start of US county confirmed time series
@@ -189,7 +189,7 @@ dfC_US_county
 ### End of US county confirmed time series
 
 
-# In[369]:
+# In[410]:
 
 
 ### Start of US county confirmed per 100k residents time series
@@ -209,12 +209,21 @@ dfC_countyPerCap.to_csv("US_County_TimeSeries_COVID19_ConfirmedPer100k.csv", ind
 # Limiting Data size
 dfC_countyPerCap_limited = dfC_countyPerCap.copy().transpose()
 dfC_countyPerCap_limited = pd.merge(dfC_countyPerCap_limited, df_USPop_county, left_index=True, right_index=True, how="inner")
-selectedStates = ['Connecticut']
-dfC_countyPerCap_limited = dfC_countyPerCap_limited.loc[dfC_countyPerCap_limited['Province_State'].isin(selectedStates)]
+selectedStates = ["Connecticut"]
+dfC_countyPerCap_limited = dfC_countyPerCap_limited.loc[dfC_countyPerCap_limited["Province_State"].isin(selectedStates)]
 dfC_countyPerCap_limited = dfC_countyPerCap_limited.set_index("Admin2")
 dfC_countyPerCap_limited = dfC_countyPerCap_limited.drop(columns = ["Combined_Key", "Population", "Province_State"])
-dfC_countyPerCap_limited.to_csv("US_CT_TimeSeries_COVID19_ConfirmedPer100k.csv", index_label="County")
-
+dfC_countyPerCap_limited = dfC_countyPerCap_limited.transpose()
+#dfC_countyPerCap_limited.to_csv("US_CT_TimeSeries_COVID19_ConfirmedPer100k.csv", index_label="County")
+dfC_countyPerCap_limited = dfC_countyPerCap_limited.rename(columns={
+    'Fairfield': 'Confirmed_Fairfield', 
+    'Hartford': 'Confirmed_Hartford', 
+    'Litchfield': 'Confirmed_Litchfield', 
+    'Middlesex': 'Confirmed_Middlesex', 
+    'New Haven': 'Confirmed_New Haven', 
+    'New London': 'Confirmed_New London', 
+    'Tolland': 'Confirmed_Tolland', 
+    'Windham': 'Confirmed_Windham'})
 
 dfC_countyPerCap_limited
 #df_USPop_county
@@ -222,7 +231,7 @@ dfC_countyPerCap_limited
 ### End of US county confirmed per 100k residents time series
 
 
-# In[370]:
+# In[400]:
 
 
 ### Start of US county deaths time series
@@ -259,7 +268,7 @@ dfD_US_county
 ### End of US county deaths time series
 
 
-# In[371]:
+# In[414]:
 
 
 ### Start of US county deaths per 100k residents time series
@@ -283,17 +292,30 @@ selectedStates = ['Connecticut']
 dfD_countyPerCap_limited = dfD_countyPerCap_limited.loc[dfD_countyPerCap_limited['Province_State'].isin(selectedStates)]
 dfD_countyPerCap_limited = dfD_countyPerCap_limited.set_index("Admin2")
 dfD_countyPerCap_limited = dfD_countyPerCap_limited.drop(columns = ["Combined_Key", "Population", "Province_State"])
-dfD_countyPerCap_limited.to_csv("US_CT_TimeSeries_COVID19_DeathsPer100k.csv", index_label="County")
+dfD_countyPerCap_limited = dfD_countyPerCap_limited.transpose()
+#dfD_countyPerCap_limited.to_csv("US_CT_TimeSeries_COVID19_DeathsPer100k.csv", index_label="County")
+dfD_countyPerCap_limited = dfD_countyPerCap_limited.rename(columns={
+    'Fairfield': 'Deaths_Fairfield', 
+    'Hartford': 'Deaths_Hartford', 
+    'Litchfield': 'Deaths_Litchfield', 
+    'Middlesex': 'Deaths_Middlesex', 
+    'New Haven': 'Deaths_New Haven', 
+    'New London': 'Deaths_New London', 
+    'Tolland': 'Deaths_Tolland', 
+    'Windham': 'Deaths_Windham'})
 
+df_countyPerCap_limited_final = pd.merge(dfC_countyPerCap_limited, dfD_countyPerCap_limited, left_index=True, right_index=True, how="inner")
+df_countyPerCap_limited_final.to_csv("US_CTCombined_TimeSeries_COVID19_DeathsPer100k.csv", index_label="County")
 
-dfD_countyPerCap_limited
+df_countyPerCap_limited_final
 
+#dfD_countyPerCap_limited
 #dfD_countyPerCap
 #df_USPop_county
 ### End of US county deaths per 100k residents time series
 
 
-# In[372]:
+# In[402]:
 
 
 ### Start of US states cross section of latest data
@@ -338,7 +360,7 @@ df_cross_section
 ### End of US states cross section of latest data
 
 
-# In[373]:
+# In[403]:
 
 
 ### Start of US county cross section of latest date
@@ -377,7 +399,7 @@ df_cross_section.to_csv("US_County_CrossSection_COVID19_Deaths.csv")
 df_cross_section
 
 
-# In[374]:
+# In[404]:
 
 
 get_ipython().system('jupyter nbconvert --to script master_us_data_cleaner.ipynb')
